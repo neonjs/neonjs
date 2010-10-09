8b7b857c7b5064c1e48d31bbd96e0afd4984cfa5
Thomas Rutter <trutter@pack>
2010-10-10 00:07:30 +1100

    real start on richtext support

--- SPARK.richtext.js
+++ SPARK.richtext.js
@@ -14,11 +14,40 @@
 */
 
 SPARK.richText = SPARK.richText || (function() {
-	
-	var startrichtext = function(element) {
-		
-	}
-	
 
+	return function(opts) {
+		var
+			i,
+			area,
+			el,
+			textareas = [];
+		
+		for (i = this.length; i--;) {
+			if (this[i].tagName.toLowerCase() == 'textarea') {
+				textareas.push((el = SPARK.select(this[i])));
+				area = el.insert({div:""}).
+					style('minWidth', el.getStyle('width')).
+					style('minHeight', el.getStyle('height'));
+				area[0].innerHTML = el[0].value;
+				el.insert({
+					input:'',
+					$type:'hidden',
+					$name:el[0].name,
+					$value:''
+					});
+				el.remove();
+			}
+			else {
+				area = SPARK.select(this[i]);
+			}
+			area.setAttribute('contenteditable', 'true').
+				style('border', '1px inset #aaa').
+				style('padding', '1px').
+				style('maxHeight', '26em').
+				style('overflow', 'auto').
+				addClass('SPARK-richtext-area');
+		}
+		
+	};
 	
 }());

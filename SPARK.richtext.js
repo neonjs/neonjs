0a78fc47520680e8b65a1251c6ff62dc820dfc7e
Thomas Rutter <trutter@pack>
2010-10-11 01:17:07 +1100

    richtext unfinished

--- SPARK.richtext.js
+++ SPARK.richtext.js
@@ -20,26 +20,79 @@ SPARK.richText = SPARK.richText || function(opts) {
 			i,
 			el,
 			container,
+			editor,
 			toolbar,
 			savebar,
-			div,
 			source,
-			canedit;
+			canedit = document.body.contentEditable == undefined;
+
+		/*
+		var totext = function(html) {
+
+
+			var 
+				matches,
+				output = '',
+				lastpos = 0,
+				mybreak = 0,
+				regex = /\s*+<\/?+(\w++)[^>"]*(?:"[^"]*"[^>"]*+)*+>\s*|\x20\s+|\t\s*|\n\s*|\r\s*|<!(?:--[\S\s]*?--|[^-][^>]*)>\s*+/;
+			while (matches = regex.exec(html)) {
+				output .= html.substr(lastpos, regex.lastIndex - lastpos);
+				if (regex.lastIndex > lastpos) {
+					mybreak = 0;
+				}
+				if ($matches[1] == '') {
+					mybreak = Math.max(mybreak, 1);
+				}
+				else {
+					if (/^(p|ul|ol|li|blockquote|h[1-6])$/i.test(matches[1])) {
+						mybreak = Math.max(mybreak, 3);
+					}
+					else {
+						if (matches[1] == 'br' || matches[1] == 'tr') {
+							mybreak = Math.max(mybreak, 2);
+						}
+						else {
+							mybreak = Math.max(mybreak, 1);
+						}
+					}
+				}
+				lastpos = regex.lastIndex;
+			}
+
+
+
+			return html;
+		};
+		*/
 
 		for (var i = that.length; i--; ) {
 			el = SPARK.select(that[i]);
+
+			// set up editor
 			container = el.insert({div:''});
-			canedit = container.contenteditable !== null;
+			editor = container.insert(canedit ? {div:''} : {textarea:''}).
+				setAttribute('contenteditable', 'true').
+				addClass('SPARK-richtext-editor').
+				style('border', '1px inset #aaa').
+				style('padding', '1px').
+				style('maxHeight', '26em').
+				style('overflow', 'auto');
+			if (!canedit) {
+				editor.style('width', '98%').
+					style('background', 'transparent').
+					style('minHeight', '14em').
+					style('color', 'inherit');
+			}
+			toolbar = editor.insert({div:''}).
+				addClass('SPARK-richtext-toolbar');
 
-
-
-
-
+			// transfer to new editor and remove old
 			if (el[0].tagName.toLowerCase() == 'textarea') {
-				div = container.append({div:''}).
+				source = el[0].value;
+				editor.
 					style('minWidth', el.getStyle('width')).
 					style('minHeight', el.getStyle('height'));
-				div[0].innerHTML = el[0].value;
 				el.insert({
 					input:'',
 					$type:'hidden',
@@ -48,17 +101,12 @@ SPARK.richText = SPARK.richText || function(opts) {
 					});
 			}
 			else {
-				div = container.append(el);
+				source = el[0].innerHTML;
 			}
 			el.remove();
-			div.setAttribute('contenteditable', 'true').
-				style('border', '1px inset #aaa').
-				style('padding', '1px').
-				style('maxHeight', '26em').
-				style('overflow', 'auto').
-				addClass('SPARK-richtext-area');
-			toolbar = div.insert({div:''}).
-				addClass('SPARK-richtext-toolbar');
+
+			editor[0][canedit ? 'innerHTML' : 'value'] = canedit ? source :
+				totext(source);
 		}
 	}
 	
